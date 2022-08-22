df6a5f7808388d478960e8f9b6de50da2403775a
Kent <kenttechnical@gmail.com>
2022-08-22 14:56:22 +0200

    Classes and class methods, method chaining

--- script.js
+++ script.js
@@ -22,3 +22,45 @@ const userTwo = {
         console.log("Logged in")
     }
 }
+
+// Classes can be used to create object blueprints
+// Allowing easier creation of similar objects
+// It would seem that class naming convention is to use upper case for the first letter ie class User but not class user
+// Properties go inside the constructor, methods go after the constructor
+// Do not use arrow functions when creating class methods, otherwise the this keyword will refer to the window object 
+// and not the object instance
+
+// Making Methods chainable, just like promises, you can chain methods as long as you return this inside the method
+
+class User {
+    constructor(name,gender,age){
+        this.name = name
+        this.gender = gender
+        this.age = age
+        this.score = 0
+        
+    }
+    login(){
+        console.log(`${this.name} logged in`)
+    }
+    logout(){
+        console.log(`${this.name} logged out`)
+    }
+    increaseScore(){
+        this.score ++
+        console.log(`${this.name}'s score is ${this.score}`)
+        return this
+    }
+}
+
+const userThree = new User ("Max","M",10)
+const userFour = new User ("Ashleigh","F",27)
+
+console.log(userThree)
+console.log(userFour)
+
+userThree.login()
+userThree.logout()
+
+// Making Methods chainable, just like promises, you can chain methods as long as you return this inside the method
+userThree.increaseScore().increaseScore().increaseScore()
